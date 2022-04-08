20b2a871334e500bc5322633c0b304b18efde409
Алмаз <almgal353@gmail.com>
2022-04-08 14:01:25 +0500

    Added entities classes. Changed project file structure.

---
+++ Auto Repair Shop/Windows/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace Auto_Repair_Shop.Windows
+{
+    /// <summary>
+    /// Логика главного окна, выводящего текущие заказы. 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Конструктор класса. 
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
